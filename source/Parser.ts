da39cd9d07540eaa699168ad47cd89ebdbe674d7
Simon Mika <simon@mika.se>
2017-12-27 16:03:11 +0100

    Removed async.

--- source/Parser.ts
+++ source/Parser.ts
@@ -4,5 +4,5 @@ import * as SiteTree from "./SiteTree"
 
 export abstract class Parser {
 	abstract readonly extensions: { [extension: string]: "ascii" | "base64" | "binary" | "hex" | "ucs2" | "utf16le" | "utf8" | undefined }
-	abstract async parse(file: Filesystem.File, handler: Error.Handler): Promise<SiteTree.Item | undefined>
+	abstract parse(file: Filesystem.File, handler: Error.Handler): Promise<SiteTree.Item | undefined>
 }
