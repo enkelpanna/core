faa20ab8489eb79f23f4db171651b19863c500c3
Simon Mika <simon@mika.se>
2017-12-28 13:54:03 +0100

    Added sourceType property to video.

--- source/SiteTree/Block/Video.ts
+++ source/SiteTree/Block/Video.ts
@@ -3,6 +3,22 @@ import { ContentBlock } from "./ContentBlock"
 import { Inline } from "../Inline/Inline"
 
 export class Video extends ContentBlock<Inline> {
+	get sourceType(): string | undefined {
+		let result: string | undefined
+		const match = this.source.match(/\.([a-z,A-Z,0-9]+)$/)
+		if (match && match.length > 1)
+			switch (match[1]) {
+				case "ogg":
+					result = "video/ogg"
+					break
+				case "mp4":
+					result = "video/mp4"
+					break
+				default:
+					break
+			}
+		return result
+	}
 	constructor(readonly source: string, readonly classes: string[], content: Inline[], region: Error.Region) {
 		super(content, region)
 	}
