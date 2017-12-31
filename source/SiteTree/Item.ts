6abd9248115d9b39d2ba247739e7dccaab118ad6
Simon Mika <simon@mika.se>
2017-12-31 14:28:22 +0100

    Added Item serial.

--- source/SiteTree/Item.ts
+++ source/SiteTree/Item.ts
@@ -1,8 +1,10 @@
 import { Error } from "@cogneco/mend"
-
+let serial = 0
 export abstract class Item {
+	readonly serial: number
 	abstract readonly type: string
 	constructor(readonly region: Error.Region) {
+		this.serial = serial++
 	}
 	toObject(): any & { type: string } {
 		return { type: this.type }
